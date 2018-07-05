93b3caff87ae152b69acf69a86f5f84a84158f51
Igor Seabra <igor.seabra@ccc.ufcg.edu.br>
2018-07-05 11:15:30 -0300

    fix java project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ProjetoP2_ListaPraMim
 Projeto em grupo de Laboratório de Programação 2
+
+Alunos:
+
+* Igor Santa Ritta Seabra - 117210304
---
+++ src/Facade.java
@@ -0,0 +1,12 @@
+import easyaccept.EasyAccept;
+
+public class Facade {
+
+	public static void main(String[] args) {
+		args = new String[] {"Facade",
+				"acception_tests/use_case1.txt"};
+		
+		EasyAccept.main(args);
+	}
+
+}
