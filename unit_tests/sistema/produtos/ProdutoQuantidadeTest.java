94d73c270dacac06b5f1783b2eaf3070e4712bee
wesleyroseno <37702941+wesleyroseno@users.noreply.github.com>
2018-07-11 21:46:58 -0300

    Atualização da função testAtualizaItem() e novos testes.
    Ainda em desenvolvimento

--- unit_tests/sistema/produtos/ProdutoQuantidadeTest.java
+++ unit_tests/sistema/produtos/ProdutoQuantidadeTest.java
@@ -18,13 +18,51 @@ public class ProdutoQuantidadeTest {
 	 */
 	@Test
 	public void testAtualizaItem() {
+		ProdutoQuantidade p1 = new ProdutoQuantidade(1, "uva", "alimento industrializado", 500, "g", "bem legal", 10);
+		
+		assertEquals("1. uva, alimento industrializado, 500 g, Preco: <bem legal, R$ 10,00;>", p1.toString());
+		
+		System.out.println(p1.toString(5));
+				
+		assertEquals("1 uva, alimento industrializado, 500 g", p1.toString(0));
+		
+		p1.atualizaItem("unidade", "5");
+		
+		assertEquals("5 uva, alimento industrializado, 500 g", p1.toString(0));
+		
+		p1.atualizaItem("quantidade", "250");
+		
+		assertEquals("5 uva, alimento industrializado, 250 g", p1.toString(0));
+	
+		p1.atualizaItem("unidade de medida", "kg");
+
+		assertEquals("5 uva, alimento industrializado, 250 kg", p1.toString(0));
+		
+		p1.atualizaItem("nome", "feijao");
+
+		assertEquals("5 feijao, alimento industrializado, 250 kg", p1.toString(0));
+		
+		p1.atualizaItem("categoria", "alimento nao industrializado");
+
+		assertEquals("5 feijao, alimento nao industrializado, 250 kg", p1.toString(0));
+		
+		p1.atualizaItem("categoria", "limpeza");
+
+		assertEquals("5 feijao, limpeza, 250 kg", p1.toString(0));
+		
+		p1.atualizaItem("categoria", "higiene pessoal");
+
+		assertEquals("5 feijao, higiene pessoal, 250 kg", p1.toString(0));
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.ProdutoQuantidade#toString(int)}.
+	 */
+	@Test
+	public void testToStringInt() {
 		ProdutoQuantidade p1 = new ProdutoQuantidade(1, "uva", "alimento nao industrializado", 500, "g", "bem legal", 10);
 		
-		assertEquals("uva", p1.getNome());
-		
-		p1.atualizaItem("nome", "pasta de dente");
-
-		assertEquals("pasta de dente", p1.getNome());
+		assertEquals("1. uva, alimento nao industrializado, 500 g, Preco: <bem legal, R$ 10,00;>", p1.toString());
 	}
 
 	/**
@@ -38,13 +76,19 @@ public class ProdutoQuantidadeTest {
 	}
 
 	/**
+	 * Test method for {@link sistema.produtos.ProdutoQuantidade#ProdutoQuantidade(sistema.produtos.ProdutoQuantidade)}.
+	 */
+	@Test
+	public void testProdutoQuantidadeProdutoQuantidade() {
+		fail("Not yet implemented");
+	}
+
+	/**
 	 * Test method for {@link sistema.produtos.ProdutoQuantidade#ProdutoQuantidade(int, java.lang.String, java.lang.String, int, java.lang.String, java.lang.String, double)}.
 	 */
 	@Test
-	public void testProdutoQuantidade() {
-		ProdutoQuantidade p1 = new ProdutoQuantidade(1, "uva", "alimento nao industrializado", 500, "g", "bem legal", 10);
-		
-		assertEquals("1. uva, alimento nao industrializado, 500 g, Preco: <bem legal, R$ 10,00;>", p1.toString());
+	public void testProdutoQuantidadeIntStringStringIntStringStringDouble() {
+		fail("Not yet implemented");
 	}
 
 }
