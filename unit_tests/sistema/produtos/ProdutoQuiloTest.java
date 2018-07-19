749cae1b084499c4ed8ead4a38e7d749762826c4
wesleyroseno <37702941+wesleyroseno@users.noreply.github.com>
2018-07-19 00:33:40 -0300

    Modificação desses testes JUnit. Faltando 3 correções em cada.
    As correções ficaram para serem analisadas em grupo, e após analise dos testes de aceitação.

--- unit_tests/sistema/produtos/ProdutoQuiloTest.java
+++ unit_tests/sistema/produtos/ProdutoQuiloTest.java
@@ -37,15 +37,6 @@ public class ProdutoQuiloTest {
 	}
 
 	/**
-	 * Test method for {@link sistema.produtos.ProdutoQuilo#toString(int)}.
-	 */
-	@Test
-	public void testToStringInt() {
-		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);		
-		assertEquals("500,00 kg uva, alimento industrializado", p1.toString(0));
-	}
-
-	/**
 	 * Test method for {@link sistema.produtos.ProdutoQuilo#toStringValues()}.
 	 */
 	@Test
@@ -55,28 +46,99 @@ public class ProdutoQuiloTest {
 	}
 
 	/**
-	 * Test method for {@link sistema.produtos.ProdutoQuilo#ProdutoQuilo(sistema.produtos.ProdutoQuilo)}.
-	 */
-	@Test
-	public void testProdutoQuiloProdutoQuilo() {		
-		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);		
-		ProdutoQuilo p2 = new ProdutoQuilo(p1);
-		
-		assertEquals("1. uva, alimento industrializado, Preco por quilo: <bem legal, R$ 10,00;>", p2.toString());		
-		assertEquals("500,00 kg uva, alimento industrializado", p2.toString(0));
-		
-		
-	}
-
-	/**
 	 * Test method for {@link sistema.produtos.ProdutoQuilo#ProdutoQuilo(int, java.lang.String, java.lang.String, double, java.lang.String, double)}.
 	 */
 	@Test
-	public void testProdutoQuiloIntStringStringDoubleStringDouble() {
+	public void testProdutoQuilo() {
 		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
 		
 		assertEquals("1. uva, alimento industrializado, Preco por quilo: <bem legal, R$ 10,00;>", p1.toString());		
 		assertEquals("500,00 kg uva, alimento industrializado", p1.toString(0));
 	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#adicionaPrecoItem(java.lang.String, double)}.
+	 */
+	@Test
+	public void testAdicionaPrecoItem() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		p1.adicionaPrecoItem("pouco legal", 8.99);
+		
+		assertEquals("1. uva, alimento industrializado, Preco por quilo: <pouco legal, R$ 8,99;bem legal, R$ 10,00;>", p1.toString());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		
+		assertEquals(1, p1.getId());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getNome()}.
+	 */
+	@Test
+	public void testGetNome() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		
+		assertEquals("uva", p1.getNome());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getCategoria()}.
+	 */
+	@Test
+	public void testGetCategoria() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		
+		assertEquals("alimento industrializado", p1.getCategoria());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getMenorPreco()}.
+	 */
+	@Test
+	public void testGetMenorPreco() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);		
+		p1.adicionaPrecoItem("pouco legal", 8.99);
+
+		
+		assertEquals(8.99, p1.getMenorPreco(), 0.005);
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#contemStringPesquisada(java.lang.String)}.
+	 */
+	@Test
+	public void testContemStringPesquisada() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+
+		assertTrue(p1.contemStringPesquisada("uva"));
+		assertFalse(p1.contemStringPesquisada("banana"));
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		
+		assertEquals("1. uva, alimento industrializado, Preco por quilo: <bem legal, R$ 10,00;>", p1.toString());	
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#toString(int)}.
+	 */
+	@Test
+	public void testToStringInt() {
+		ProdutoQuilo p1 = new ProdutoQuilo(1, "uva", "alimento industrializado", 500.00, "bem legal", 10.0);
+		
+		assertEquals("500,00 kg uva, alimento industrializado", p1.toString(0));
+	}
+
 
 }
--- unit_tests/sistema/produtos/ProdutoUnidadeTest.java
+++ unit_tests/sistema/produtos/ProdutoUnidadeTest.java
@@ -37,16 +37,6 @@ public class ProdutoUnidadeTest {
 	}
 
 	/**
-	 * Test method for {@link sistema.produtos.ProdutoUnidade#toString(int)}.
-	 */
-	@Test
-	public void testToStringInt() {
-		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
-		
-		assertEquals("500 uva, alimento industrializado", p1.toString(0));
-	}
-
-	/**
 	 * Test method for {@link sistema.produtos.ProdutoUnidade#toStringValues()}.
 	 */
 	@Test
@@ -57,25 +47,97 @@ public class ProdutoUnidadeTest {
 	}
 
 	/**
-	 * Test method for {@link sistema.produtos.ProdutoUnidade#ProdutoUnidade(sistema.produtos.ProdutoUnidade)}.
-	 */
-	@Test
-	public void testProdutoUnidadeProdutoUnidade() {
-		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
-		ProdutoUnidade p2 = new ProdutoUnidade(p1);
-		
-		assertEquals("1. uva, alimento industrializado, Preco: <bem legal, R$ 10,00;>", p2.toString());		
-		assertEquals("500 uva, alimento industrializado", p2.toString(0));
-	}
-
-	/**
 	 * Test method for {@link sistema.produtos.ProdutoUnidade#ProdutoUnidade(int, java.lang.String, java.lang.String, int, java.lang.String, double)}.
 	 */
 	@Test
-	public void testProdutoUnidadeIntStringStringIntStringDouble() {
+	public void testProdutoUnidade() {
 		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
 		
 		assertEquals("1. uva, alimento industrializado, Preco: <bem legal, R$ 10,00;>", p1.toString());		
+		assertEquals("500 uva, alimento industrializado", p1.toString(0));
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#adicionaPrecoItem(java.lang.String, double)}.
+	 */
+	@Test
+	public void testAdicionaPrecoItem() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		p1.adicionaPrecoItem("pouco legal", 8.99);
+		
+		assertEquals("1. uva, alimento industrializado, Preco: <pouco legal, R$ 8,99;bem legal, R$ 10,00;>", p1.toString());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getId()}.
+	 */
+	@Test
+	public void testGetId() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
+		assertEquals(1, p1.getId());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getNome()}.
+	 */
+	@Test
+	public void testGetNome() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
+		assertEquals("uva", p1.getNome());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getCategoria()}.
+	 */
+	@Test
+	public void testGetCategoria() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
+		assertEquals("alimento industrializado", p1.getCategoria());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#getMenorPreco()}.
+	 */
+	@Test
+	public void testGetMenorPreco() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);		
+		p1.adicionaPrecoItem("pouco legal", 8.99);
+
+		
+		assertEquals(8.99, p1.getMenorPreco(), 0.005);
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#contemStringPesquisada(java.lang.String)}.
+	 */
+	@Test
+	public void testContemStringPesquisada() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
+		assertTrue(p1.contemStringPesquisada("uva"));
+		assertFalse(p1.contemStringPesquisada("banana"));
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
+		assertEquals("1. uva, alimento industrializado, Preco: <bem legal, R$ 10,00;>", p1.toString());
+	}
+
+	/**
+	 * Test method for {@link sistema.produtos.Produto#toString(int)}.
+	 */
+	@Test
+	public void testToStringInt() {
+		ProdutoUnidade p1 = new ProdutoUnidade(1, "uva", "alimento industrializado", 500, "bem legal", 10.0);
+		
 		assertEquals("500 uva, alimento industrializado", p1.toString(0));
 	}
 
